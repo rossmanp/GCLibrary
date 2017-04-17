521ce91ff23e5fbabc2a16af3dafcdb3a84f3798
Pete Rossman <pete.rossman@gmail.com>
2017-04-17 13:49:38 -0400

    First functional version of methods CheckoutBook and ReturnBook

--- GCLibrary/Book.cs
+++ GCLibrary/Book.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace GCLibrary
 {
-    class Book
+    public class Book
     {
         public string title { get; private set; }
         public bool checkedOut { get; set; }
 
-        Book()
+        public Book(string title, bool checkedOut)
         {
-            
+            this.title = title;
+            this.checkedOut = checkedOut;
         }
 
     }
